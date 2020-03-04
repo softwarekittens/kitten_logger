42c1f31b21f0b201ba5b675415db8f52801f07b1
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2020-03-04 13:41:19 +0300

    disable tracing, release logs

--- kitten_logger/logger.h
+++ kitten_logger/logger.h
@@ -2,14 +2,34 @@
 
 #include <boost/log/trivial.hpp>
 
-#define POSTFIX __FILE__ << " : " <<  __func__ << " : " << __LINE__
+#define POSTFIX "[" << __FILE__ << ":" << __LINE__ << ", " <<  __func__ << "] "
 #define LOG_WRAPPER(level) BOOST_LOG_TRIVIAL(level) << POSTFIX
+
+#ifdef NDEBUG
+
+struct EmptyLogLine {
+
+    template <class T>
+    EmptyLogLine& operator<< (const T&) {
+        return *this;
+    }
+};
+
+// log trace level
+#define LOGT EmptyLogLine()
+
+// log debug level
+#define LOGD EmptyLogLine()
+
+#else
 
 // log trace level
 #define LOGT LOG_WRAPPER(trace)
 
 // log debug level
 #define LOGD LOG_WRAPPER(debug)
+
+#endif
 
 // log info level
 #define LOGI LOG_WRAPPER(info)
--- kitten_logger/trace.h
+++ kitten_logger/trace.h
@@ -2,12 +2,13 @@
 
 #include <string_view>
 
-#ifndef DISABLE_TRACING
+#ifdef ENABLE_TRACING
 #define TRACE_SCOPE kitten::ScopedTrace __traceMarker__##__LINE__(__FILE__, __func__, __LINE__)
 #define TRACE_NAMED_SCOPE(name) kitten::ScopedTrace __traceMarker__##__LINE__(__FILE__, name, __LINE__)
 #define TRACE_SET_THREAD_NAME(name) kitten::setTraceThreadName(name)
 #else
 #define TRACE_SCOPE
+#define TRACE_NAMED_SCOPE(name)
 #define TRACE_SET_THREAD_NAME(name) 
 #endif
 
