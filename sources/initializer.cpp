d60ea286bb078ecf32b505dc0793d3a9d7ecea7d
klyashkevich <klyashkevich@emtc.ru>
2020-06-08 00:31:14 +0000

    few fixes

--- sources/initializer.cpp
+++ sources/initializer.cpp
@@ -6,12 +6,14 @@ INITIALIZE_EASYLOGGINGPP
 
 namespace {
 
+#ifndef WITHOUT_CRASH_HANDLER
 void crashHandler(int sig) {
     LOGE << "Woops! Crashed! signal " << sig;     
     LOGE << boost::stacktrace::stacktrace();
     // FOLLOWING LINE IS ABSOLUTELY NEEDED AT THE END IN ORDER TO ABORT APPLICATION
     el::Helpers::crashAbort(sig);
 }
+#endif //WITHOUT_CRASH_HANDLER
 
 struct LoggerInitializer {
     LoggerInitializer() {
@@ -19,6 +21,9 @@ struct LoggerInitializer {
         defaultConf.setToDefault();
         defaultConf.setGlobally(el::ConfigurationType::Format, "[%datetime{%Y-%M-%d %H:%m:%s.%g}] [%thread] [%level] [%fbase:%line] %msg");
         el::Loggers::reconfigureLogger("default", defaultConf);
+#ifndef WITHOUT_CRASH_HANDLER
+        el::Helpers::setCrashHandler(crashHandler);
+#endif //WITHOUT_CRASH_HANDLER
     }
 };
 
